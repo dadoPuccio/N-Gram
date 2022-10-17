89f9dbbd14d3fb261e6624f6789f671aa0c14f2a
dadoPuccio <d.pucci98@gmail.com>
2022-10-17 18:07:41 +0200

    Added tests' structure and some utilities to create files with a fixed amount of words

--- Utils.h
+++ Utils.h
@@ -9,6 +9,8 @@
 
 bool processInputChar(char& c);
 std::string mergeBooks(std::string dir, std::string file_name);
+std::string createFileWith_M_Words(std::string dir, std::string file_name, int M);
+int countWords(std::string& file_path);
 
 bool processInputChar(char& c){
     /* Processes the input, removing everything apart alphabet letters and spaces */
@@ -40,5 +42,95 @@ std::string mergeBooks(std::string dir, std::string file_name){
 
     return dir + file_name;
 }
+
+std::string createFileWithMwords(std::string dir, std::string file_name, int M){
+    std::ifstream book;
+    std::string line;
+    std::string tmp;
+    size_t pos;
+
+    std::ofstream MwordsBook;
+    MwordsBook.open(dir + file_name);
+
+    int counter = 0;
+
+    while(counter < M) {
+        for (const auto &entry: std::filesystem::directory_iterator(dir)) {
+            if (std::filesystem::file_size(entry.path()) > 0 &&
+                entry.path() != dir + file_name) { // as empty files might be created with large chunk sizes
+                book.open(entry.path());
+
+                while (std::getline(book, line)) {
+
+                    if (counter <= M)
+                        MwordsBook << line << std::endl;
+                    else
+                        break;
+
+                    pos = line.find(' ');
+                    while (pos != std::string::npos) {
+                        if (pos > 0) // in such a way we ignore double spaces
+                            counter += 1;
+                        line.erase(0, pos + 1); // erase the space as well
+                        pos = line.find(' ');
+                    }
+
+                    // check if there is another word at the end of the line
+                    if (!line.empty()) {
+                        tmp = line.substr(0, line.length() - 1);
+                        if (!tmp.empty())
+                            counter += 1;
+                    }
+                }
+
+                book.close();
+
+                if (counter > M)
+                    break;
+            }
+        }
+    }
+    MwordsBook.close();
+    std::cout << "This file has approx " << counter << " words" <<  std::endl;
+
+    return dir + file_name;
+}
+
+int countWords(std::string& file_path){
+    std::ifstream book;
+    book.open(file_path);
+
+    std::string line;
+    std::string tmp;
+    size_t pos;
+
+    int counter = 0;
+
+    while (std::getline(book, line)) {
+
+        pos = line.find(' ');
+        while (pos != std::string::npos) {
+            if (pos > 0) // in such a way we ignore double spaces
+                counter += 1;
+            line.erase(0, pos + 1); // erase the space as well
+            pos = line.find(' ');
+        }
+
+        // check if there is another word at the end of the line
+        if (!line.empty()) {
+            tmp = line.substr(0, line.length() - 1);
+            if (!tmp.empty())
+                counter += 1;
+        }
+    }
+
+    book.close();
+
+    std::cout << "This file has approx " << counter << " words" <<  std::endl;
+
+    return counter;
+}
+
+
 
 #endif //N_GRAM_UTILS_H
--- main.cpp
+++ main.cpp
@@ -8,12 +8,17 @@
 
 int main() {
 
-    int n = 2; // n-gram
-    int chunk_size = 3000; // chunk_size of jobs in the parallel implementation
-
-    std::string file_name = "../../Testi/Proust.txt";
+    std::string file_name;
 
     // file_name = mergeBooks("../../Testi/", "MergedBooks.txt");
+
+    // file_name = createFileWithMwords("../../Testi/", "1000.txt", 1000);
+    // file_name = createFileWithMwords("../../Testi/", "10000.txt", 10000);
+    // file_name = createFileWithMwords("../../Testi/", "100000.txt", 100000);
+    // file_name = createFileWithMwords("../../Testi/", "1M.txt", 1000000);
+    // file_name = createFileWithMwords("../../Testi/", "10M.txt", 10000000);
+    // file_name = createFileWithMwords("../../Testi/", "100M.txt", 100000000);
+
 
     std::string out_folder_sequential = "../OutSequential/";
     std::string out_folder_parallel = "../OutParallel/";
@@ -30,19 +35,51 @@ int main() {
         std::filesystem::create_directory(out_folder_parallel); // create the folder if it doesn't exist
     }
 
-    // Sequential Solution
-    std::cout << "Sequential solution..." << std::endl;
-    double start = omp_get_wtime();
-    sequentialNgramWords(file_name, out_folder_sequential, n);
-    double end = omp_get_wtime( );
-    std::cout << "Time: " << end-start << std::endl << std::endl;
+    int n_runs = 50;
+    int n = 2; // n-gram
+    int chunk_size = 1000; // chunk_size of jobs in the parallel implementation
+    int n_threads = 8;
 
-    // Parallel Solution
-    std::cout << "Parallel solution..." << std::endl;
-    start = omp_get_wtime();
-    parallelNgramWords(file_name, out_folder_parallel, n, chunk_size, 8);
-    end = omp_get_wtime( );
-    std::cout << "Time: " << end-start << std::endl << std::endl;
+    file_name = "../../Testi/Proust.txt";
+
+    countWords(file_name);
+
+    double start, end, s, p;
+    double avgSequentialTime, avgParallelTime, avgSpeedup;
+    avgSequentialTime = 0;
+    avgParallelTime = 0;
+    avgSpeedup = 0;
+
+    for(int i=0; i<n_runs; i++) {
+
+        // Sequential Solution
+        start = omp_get_wtime();
+        sequentialNgramWords(file_name, out_folder_sequential, n);
+        end = omp_get_wtime();
+        s = end - start;
+        avgSequentialTime += s;
+        std::cout << "Sequential Time: " << s << std::endl;
+
+        // Parallel Solution
+        start = omp_get_wtime();
+        parallelNgramWords(file_name, out_folder_parallel, n, chunk_size, n_threads);
+        end = omp_get_wtime();
+        p = end - start;
+        avgParallelTime += p;
+        std::cout << "Parallel Time: " << p << std::endl;
+
+        avgSpeedup += s/p;
+        std::cout << "Speedup: " << s/p << std::endl << std::endl;
+    }
+
+    avgSequentialTime = avgSequentialTime/n_runs;
+    avgParallelTime = avgParallelTime/n_runs;
+    avgSpeedup = avgSpeedup/n_runs;
+
+    std::cout << std::endl << "Average Sequential Time: " << avgSequentialTime << std::endl;
+    std::cout << "Average Parallel Time: " << avgParallelTime << std::endl;
+    std::cout << "Average Speedup: " << avgSpeedup << std::endl;
+    std::cout << "Average Speedup: " << avgSequentialTime/avgParallelTime << std::endl;
 
 }
 
